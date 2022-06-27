3973a02123bc6c913f3c865916634c53112a41f2
Xuefeng Wu <sirowood@gmail.com>
2022-06-27 21:06:56 +0300

    13.6 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 
 const { Blog } = require('../Models');
 
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id);
+  next();
+};
+
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll();
   res.json(blogs);
@@ -12,12 +17,21 @@ router.post('/', async (req, res) => {
   res.json(blog);
 });
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const blog = await Blog.findByPk(req.params.id);
-    await blog.destroy();
-    res.status(200).send('ok');
-  } catch (error) {
+router.put('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes;
+    await req.blog.save();
+    res.json(req.blog);
+  } else {
+    res.status(404).end();
+  };
+});
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy();
+    res.status(204).end();
+  } else {
     res.status(404).end();
   };
 });
