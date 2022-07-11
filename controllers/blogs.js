bd2677d0d9d1b7e453061b78ee9864b619833942
Xuefeng Wu <sirowood@gmail.com>
2022-07-11 19:32:30 +0300

    13.14 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -26,11 +26,18 @@ const blogFinder = async (req, res, next) => {
 };
 
 router.get('/', async (req, res) => {
-  const where = {};
+  let where = {};
 
   if (req.query.search) {
-    where.title = {
-      [Op.iLike]: `%${req.query.search}%`,
+    where = {
+      [Op.or]: [
+        {
+          title: { [Op.iLike]: `%${req.query.search}%` },
+        },
+        {
+          author: { [Op.iLike]: `%${req.query.search}%` },
+        },
+      ],
     };
   };
   const blogs = await Blog.findAll({
