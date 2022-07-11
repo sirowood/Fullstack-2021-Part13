97b6b508eb4ec8a332193f836b7cb6fdb2a239fc
Xuefeng Wu <sirowood@gmail.com>
2022-07-11 19:13:20 +0300

    13.13 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const { Blog, User } = require('../Models');
 const jwt = require('jsonwebtoken');
 const { SECRET } = require('../util/config');
+const { Op } = require('sequelize');
 
 const tokenExtractor = (req, res, next) => {
   const authorization = req.get('authorization');
@@ -25,12 +26,20 @@ const blogFinder = async (req, res, next) => {
 };
 
 router.get('/', async (req, res) => {
+  const where = {};
+
+  if (req.query.search) {
+    where.title = {
+      [Op.iLike]: `%${req.query.search}%`,
+    };
+  };
   const blogs = await Blog.findAll({
     attributes: { exclude: ['userId'] },
     include: {
       model: User,
       attributes: { exclude: ['password', 'createdAt', 'updatedAt'] }
-    }
+    },
+    where
   });
   res.json(blogs);
 });
