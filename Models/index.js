f3e65e2f7913cb7ced5948ea8d7711d7812ea980
Xuefeng Wu <sirowood@gmail.com>
2022-07-13 11:53:00 +0300

    13.20 done

--- Models/index.js
+++ Models/index.js
@@ -5,7 +5,7 @@ const ReadingList = require('./readingList');
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
-User.belongsToMany(Blog, { through: ReadingList });
+User.belongsToMany(Blog, { through: ReadingList, as: 'readings' });
 Blog.belongsToMany(User, { through: ReadingList });
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -27,6 +27,24 @@ router.get('/', async (req, res) => {
   res.json(users);
 });
 
+router.get('/:id', async (req, res) => {
+  const user = await User.findByPk(req.params.id, {
+    attributes: ['name', 'username'],
+    include: {
+      model: Blog,
+      as: 'readings',
+      attributes: {
+        exclude: ['createdAt', 'updatedAt', 'userId'],
+      },
+      through: {
+        attributes: [],
+      },
+    },
+  });
+
+  res.json(user);
+});
+
 router.put('/:username', async (req, res) => {
   const user = await User.findOne({
     where: {
